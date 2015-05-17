ef2bd7a7d27451e5400ee92a553bca79ab2938a6
yuhki50 <yuhki50@gmail.com>
2015-05-17 16:25:04 +0900

    modify

--- UsbDongle/Source/UsbDongle.c
+++ UsbDongle/Source/UsbDongle.c
@@ -107,8 +107,6 @@ void cbAppColdStart(bool_t bAfterAhiInit) {
   }
 }
 
-//static bool_t bWakeupByButton;
-
 /**
  * AppWarmStart
  * @param bAfterAhiInit
@@ -117,18 +115,7 @@ void cbAppWarmStart(bool_t bAfterAhiInit) {
   if (!bAfterAhiInit) {
     // before AHI init, very first of code.
     //  to check interrupt source, etc.
-    //bWakeupByButton = FALSE;
 
-    /*
-    if (u8AHI_WakeTimerFiredStatus()) {
-      // wake up timer
-    } else if (u32AHI_DioWakeStatus() & u32DioPortWakeUp) {
-      // woke up from DIO events
-      bWakeupByButton = TRUE;
-    } else {
-      bWakeupByButton = FALSE;
-    }
-    */
   } else {
     // initialize hardware
     vInitHardware(TRUE);
@@ -475,7 +462,7 @@ static void vProcessEvCore(tsEvent *pEv, teEvent eEvent, uint32 u32evarg) {
     }
     if (u32evarg & EVARG_START_UP_WAKEUP_MASK) {
       vfPrintf(&sSerStream, LB "Wake up by %s. SleepCt=%d",
-               "UNKNOWN", //bWakeupByButton ? "UART PORT" : "WAKE TIMER",
+               "UNKNOWN",
                sAppData.u8SleepCt);
     } else {
       vfPrintf(&sSerStream, "\r\n*** ToCoNet PINGPONG SAMPLE %d.%02d-%d ***",
