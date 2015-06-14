49595eb31e3419b6fa356663128dea24c1e28a71
yuhki50 <yuhki50@gmail.com>
2015-06-15 00:33:45 +0900

    fullcolorled

--- Ball/Source/FullColorLed.c
+++ Ball/Source/FullColorLed.c
@@ -3,8 +3,9 @@
 
 #include "FullColorLed.h"
 
+PRIVATE void vRemapLedIndex(tsFullColorLed *psFullColorLed, uint8 *u8Index);
 PRIVATE bool_t
-validateIndexRange(tsFullColorLed *psFullColorLed, uint8 u8Index);
+bValidateIndexRange(tsFullColorLed *psFullColorLed, uint8 u8Index);
 
 /**
  *
@@ -24,14 +25,12 @@ PUBLIC void vFullColorLed_setLedRaw(tsFullColorLed *psFullColorLed,
                                     uint8 u8Index, uint16 u16Red,
                                     uint16 u16Green, uint16 u16Blue,
                                     uint16 u16White) {
-  u8Index = psFullColorLed->u8LedMappingTable == NULL
-                ? u8Index
-                : psFullColorLed->u8LedMappingTable[u8Index];
+  vRemapLedIndex(psFullColorLed, &u8Index);
 
   uint8 u8DeviceIndex = u8Index / PCA9685_LED_RGBW_NUMBER;
   uint8 u8LedIndex = u8Index % PCA9685_LED_RGBW_NUMBER;
 
-  if (validateIndexRange(psFullColorLed, u8Index)) {
+  if (bValidateIndexRange(psFullColorLed, u8Index)) {
     vPCA9685_setRgbwLedRaw(&(psFullColorLed->sPCA9685s[u8DeviceIndex]),
                            u8LedIndex, u16Red, u16Green, u16Blue, u16White);
   }
@@ -43,14 +42,12 @@ PUBLIC void vFullColorLed_setLedRaw(tsFullColorLed *psFullColorLed,
  */
 PUBLIC void vFullColorLed_setLed(tsFullColorLed *psFullColorLed, uint8 u8Index,
                                  uint32 u32Color) {
-  u8Index = psFullColorLed->u8LedMappingTable == NULL
-                ? u8Index
-                : psFullColorLed->u8LedMappingTable[u8Index];
+  vRemapLedIndex(psFullColorLed, &u8Index);
 
   uint8 u8DeviceIndex = u8Index / PCA9685_LED_RGBW_NUMBER;
   uint8 u8LedIndex = u8Index % PCA9685_LED_RGBW_NUMBER;
 
-  if (validateIndexRange(psFullColorLed, u8Index)) {
+  if (bValidateIndexRange(psFullColorLed, u8Index)) {
     vPCA9685_setRgbwLed(&(psFullColorLed->sPCA9685s[u8DeviceIndex]), u8LedIndex,
                         u32Color);
   }
@@ -60,7 +57,17 @@ PUBLIC void vFullColorLed_setLed(tsFullColorLed *psFullColorLed, uint8 u8Index,
  *
  * @param
  */
+PRIVATE void vRemapLedIndex(tsFullColorLed *psFullColorLed, uint8 *u8Index) {
+  *u8Index = psFullColorLed->u8LedMappingTable == NULL
+                 ? *u8Index
+                 : psFullColorLed->u8LedMappingTable[*u8Index];
+}
+
+/**
+ *
+ * @param
+ */
 PRIVATE bool_t
-validateIndexRange(tsFullColorLed *psFullColorLed, uint8 u8Index) {
+bValidateIndexRange(tsFullColorLed *psFullColorLed, uint8 u8Index) {
   return u8Index < psFullColorLed->u8PCA9685Length * PCA9685_LED_RGBW_NUMBER;
 }
