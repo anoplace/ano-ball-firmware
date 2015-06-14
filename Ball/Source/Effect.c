c1216e09d2f1a1fecd882cf048099ec230995404
yuhki50 <yuhki50@gmail.com>
2015-06-15 02:22:02 +0900

    Effect

---
+++ Ball/Source/Effect.c
@@ -0,0 +1,10 @@
+#include <jendefs.h>
+#include <AppHardwareApi.h>
+
+#include "Effect.h"
+
+/**
+ *
+ * @param
+ */
+PUBLIC void vEffect_Init(tsEffect *psEffect) {}
---
+++ Ball/Source/Effect.h
@@ -0,0 +1,19 @@
+#ifndef EFFECT_H_INCLUDED
+#define EFFECT_H_INCLUDED
+
+#include "Effect.h"
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+} tsEffect;
+
+PUBLIC void vEffect_Init(tsEffect *psEffect);
+
+#if defined __cplusplus
+}
+#endif
+
+#endif /* EFFECT_H_INCLUDED */
